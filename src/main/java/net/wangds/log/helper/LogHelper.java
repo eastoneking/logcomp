c3479a454da096ea38b9e83ae60b0c10cc56fc34
王东石 <wangds2016@163.com>
2019-11-30 15:27:24 +0800

    补充接口
    通过class指定logger的接口;
    Exception参数类型替换为Throwable类型.

--- src/main/java/net/wangds/log/helper/LogHelper.java
+++ src/main/java/net/wangds/log/helper/LogHelper.java
@@ -25,7 +25,7 @@ public final class LogHelper {
         return logger;
     }
 
-    private static String buildMsg( Supplier<String> msgFac, Exception e){
+    private static String buildMsg( Supplier<String> msgFac, Throwable e){
         String msg = "";
         if (msgFac!=null){
             msg = msgFac.get();
@@ -38,33 +38,33 @@ public final class LogHelper {
         return msg;
     }
 
-    private static void log(Supplier<Boolean> levelEnableTest, BiConsumer<String, Throwable> logFunc, Supplier<String> msgFac, Exception e){
+    private static void log(Supplier<Boolean> levelEnableTest, BiConsumer<String, Throwable> logFunc, Supplier<String> msgFac, Throwable e){
         if(levelEnableTest==null?true:levelEnableTest.get()){
             logFunc.accept(buildMsg(msgFac, e), e);
         }
     }
 
-    public static void trace(Logger logger, Supplier<String> facMsg, Exception e){
+    public static void trace(Logger logger, Supplier<String> facMsg, Throwable e){
         logger = buildLogger(logger);
         log(logger::isTraceEnabled, logger::trace, facMsg, e);
     }
 
-    public static void debug(Logger logger, Supplier<String> facMsg, Exception e){
+    public static void debug(Logger logger, Supplier<String> facMsg, Throwable e){
         logger = buildLogger(logger);
         log(logger::isDebugEnabled, logger::debug, facMsg, e);
     }
 
-    public static void info(Logger logger, Supplier<String> facMsg, Exception e){
+    public static void info(Logger logger, Supplier<String> facMsg, Throwable e){
         logger = buildLogger(logger);
         log(logger::isInfoEnabled, logger::info, facMsg, e);
     }
 
-    public static void warn(Logger logger, Supplier<String> facMsg, Exception e){
+    public static void warn(Logger logger, Supplier<String> facMsg, Throwable e){
         logger = buildLogger(logger);
         log(logger::isWarnEnabled, logger::warn, facMsg, e);
     }
 
-    public static void error(Logger logger, Supplier<String> facMsg, Exception e){
+    public static void error(Logger logger, Supplier<String> facMsg, Throwable e){
         logger = buildLogger(logger);
         log(logger::isErrorEnabled, logger::error, facMsg, e);
     }
@@ -94,52 +94,127 @@ public final class LogHelper {
         log(logger::isErrorEnabled, logger::error, facMsg, null);
     }
 
-    public static void trace(Logger logger, Exception e){
+    public static void trace(Logger logger, Throwable e){
         logger = buildLogger(logger);
         log(logger::isTraceEnabled, logger::trace, null, e);
     }
 
-    public static void debug(Logger logger, Exception e){
+    public static void debug(Logger logger, Throwable e){
         logger = buildLogger(logger);
         log(logger::isDebugEnabled, logger::debug, null, e);
     }
 
-    public static void info(Logger logger, Exception e){
+    public static void info(Logger logger, Throwable e){
         logger = buildLogger(logger);
         log(logger::isInfoEnabled, logger::info, null, e);
     }
 
-    public static void warn(Logger logger, Exception e){
+    public static void warn(Logger logger, Throwable e){
         logger = buildLogger(logger);
         log(logger::isWarnEnabled, logger::warn, null, e);
     }
 
-    public static void error(Logger logger, Exception e){
+    public static void error(Logger logger, Throwable e){
         logger = buildLogger(logger);
         log( logger::isErrorEnabled, logger::error, null, e);
     }
 
-    public static void trace(Supplier<String> facMsg, Exception e){
+    public static void trace(Class clz, Supplier<String> facMsg, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isTraceEnabled, logger::trace, facMsg, e);
+    }
+
+    public static void debug(Class clz, Supplier<String> facMsg, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isDebugEnabled, logger::debug, facMsg, e);
+    }
+
+    public static void info(Class clz, Supplier<String> facMsg, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isInfoEnabled, logger::info, facMsg, e);
+    }
+
+    public static void warn(Class clz, Supplier<String> facMsg, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isWarnEnabled, logger::warn, facMsg, e);
+    }
+
+    public static void error(Class clz, Supplier<String> facMsg, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isErrorEnabled, logger::error, facMsg, e);
+    }
+
+    public static void trace(Class clz, Supplier<String> facMsg){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isTraceEnabled, logger::trace, facMsg, null);
+    }
+
+    public static void debug(Class clz, Supplier<String> facMsg){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isDebugEnabled, logger::debug, facMsg, null);
+    }
+
+    public static void info(Class clz, Supplier<String> facMsg){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isInfoEnabled, logger::info, facMsg, null);
+    }
+
+    public static void warn(Class clz, Supplier<String> facMsg){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isWarnEnabled, logger::warn, facMsg, null);
+    }
+
+    public static void error(Class clz, Supplier<String> facMsg){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isErrorEnabled, logger::error, facMsg, null);
+    }
+
+    public static void trace(Class clz, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isTraceEnabled, logger::trace, null, e);
+    }
+
+    public static void debug(Class clz, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isDebugEnabled, logger::debug, null, e);
+    }
+
+    public static void info(Class clz, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isInfoEnabled, logger::info, null, e);
+    }
+
+    public static void warn(Class clz, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log(logger::isWarnEnabled, logger::warn, null, e);
+    }
+
+    public static void error(Class clz, Throwable e){
+        Logger logger = LoggerFactory.getLogger(clz);
+        log( logger::isErrorEnabled, logger::error, null, e);
+    }
+
+    public static void trace(Supplier<String> facMsg, Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isTraceEnabled, logger::trace, facMsg, e);
     }
 
-    public static void debug(Supplier<String> facMsg, Exception e){
+    public static void debug(Supplier<String> facMsg, Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isDebugEnabled, logger::debug, facMsg, e);
     }
 
-    public static void info(Supplier<String> facMsg, Exception e){
+    public static void info(Supplier<String> facMsg, Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isInfoEnabled, logger::info, facMsg, e);
     }
 
-    public static void warn(Supplier<String> facMsg, Exception e){
+    public static void warn(Supplier<String> facMsg, Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isWarnEnabled, logger::warn, facMsg, e);
     }
 
-    public static void error(Supplier<String> facMsg, Exception e){
+    public static void error(Supplier<String> facMsg, Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isErrorEnabled, logger::error, facMsg, e);
     }
@@ -171,27 +246,27 @@ public final class LogHelper {
 
     private static Supplier<String> defaultNullMsgFac = ()->null;
 
-    public static void trace(Exception e){
+    public static void trace(Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isTraceEnabled, logger::trace, defaultNullMsgFac, e);
     }
 
-    public static void debug(Exception e){
+    public static void debug(Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isDebugEnabled, logger::debug, defaultNullMsgFac, e);
     }
 
-    public static void info(Exception e){
+    public static void info(Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isInfoEnabled, logger::info, defaultNullMsgFac, e);
     }
 
-    public static void warn(Exception e){
+    public static void warn(Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isWarnEnabled, logger::warn, defaultNullMsgFac, e);
     }
 
-    public static void error(Exception e){
+    public static void error(Throwable e){
         Logger logger = buildLogger(null);
         log(logger::isErrorEnabled, logger::error, defaultNullMsgFac, e);
     }
